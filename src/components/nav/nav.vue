cbb009e29f4351ed120824ddb84f4b4b507a4559
Omar <okhayal@gmail.com>
2021-11-25 22:41:17 -0800

    added nav

--- src/components/nav/nav.vue
+++ src/components/nav/nav.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="top-header">
 		<div class="left"> 
-			<h4 class="headline">EN</h4>
+			<div class="fflag fflag-US ff-mds ff-wave"></div>
+			<p class="noto-sans-small-bold">EN</p>
 		</div>
 		<div class="icon-bar">
-			<a class="" href="#"><i class="fa fa-home"></i></a>
-			<a href="#"><i class="fa fa-search"></i></a>
-			<a href="#"><i class="fa fa-envelope"></i></a>
-			<a href="#"><i class="fa fa-globe"></i></a>
-			<a class="checkout-icon" href="#"><i class="fa fa-trash"></i></a>
+			<a href="#"><i class="fa fa-shield"></i></a>
+			<a href="#"><i class="fa fa-question-circle"></i></a>
+			<a href="#"><i class="fa fa-map-marker"></i></a>
+			<a href="#"><i class="fa fa-user"></i></a>
+			<a class="checkout-icon" href="#"><i class="fa fa-shopping-cart"></i><p class="noto-sans-small-bold">3</p></a>
 		</div>  
 		
   </div>
@@ -20,11 +21,13 @@
 </script>
 
 <style lang="less">
-  	@import '../../styles/variables.less';
+	@import '../../styles/variables.less';
+	@import '../../styles/flags.less';
+	  
 
 	.top-header {
 		background-color: #000;
-		height: 35px;
+		height: 34px;
 		color: #fff;
 		display: flex;
 		flex-direction: row;
@@ -34,16 +37,21 @@
 		align-content: space-between;
 
 		.left {
+			padding-left: 10px;
 			display: flex;
-			justify-content: center;
-			align-content: center;
-			flex-direction: column;
-		}
+			flex-direction: row;
+			flex-wrap: nowrap;
+			justify-content: flex-start;
+			align-items: center;
+			align-content: space-around;
 
+			.fflag {
+				margin-right: 5px;
+			}
+		}
 
 		.icon-bar {
 			width: 500px;
-
 			display: flex;
 			flex-direction: row;
 			flex-wrap: nowrap;
@@ -52,15 +60,23 @@
 			align-content: stretch;
 
 			a {
-				// float: right;
-				// text-align: center;
-				padding: 10px;
+				padding: 9px 15px 10px 5px;
 				color: white;
-				//height: 100%;
 
 				&.checkout-icon {
+					padding-top: 5px;
+					padding-left: 10px;
+					text-decoration: none;
 					background-color: @primary;
 					color: #fff;
+
+					i {
+						margin-right: 5px;
+					}		
+					
+					p {
+						display: inline-block;
+					}
 				}
 			}
 
